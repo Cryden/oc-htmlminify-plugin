158ef210b7fa9785a0b756385418b6e522a92207
CRYDEsigN <Cryden_da@mail.ru>
2017-12-13 11:28:22 +0300

    plugin init

---
+++ Plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Crydesign\Htmlminify;
+
+class Plugin extends \System\Classes\PluginBase
+{
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'HTML minify Plugin',
+            'description' => 'Any HTML, inline CSS and Javascript will be compressed.',
+            'author' => 'CRYDEsigN',
+            'icon' => 'icon-compress',
+            'iconSVG' => 'plugins/crydesign/htmlminify/assets/images/logo.png'
+        ];
+    }
+
+    public function registerComponents()
+    {
+    }
+}
